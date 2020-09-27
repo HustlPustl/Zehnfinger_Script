42677acaaeab40ecfd60aed914561b4ff601f0b7
Paul Reuter <52424686+HustlPustl@users.noreply.github.com>
2020-09-27 14:05:48 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,20 @@
-import keyboard
 import time
-
-Text=""""""
+import keyboard
+text=""""""
+time.sleep(5)
+print("Keybord Eingabe startet, bitte warten")
+keyboard.write(text)
 dot="."
-
-time.sleep(3)
-print("LOOOOOS")
-keyboard.write(Text)
-time.sleep(75)
-keyboard.write(dot)
+strlen=1
+apm=0
+for c in text:
+    strlen+=1 
+zeit=0.02
+while 1:
+    apm=strlen*60/zeit
+    zeit+=0.02
+    time.sleep(0.02)
+    if apm < 670:
+        print("Eingabe zuenden")
+        keyboard.write(dot)
+        break
